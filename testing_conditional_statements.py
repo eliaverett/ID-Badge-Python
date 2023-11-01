8df06d427ba582b808a14a40b03b5d2befb29916
eliaverett <eliaverett39@gmail.com>
2023-11-01 14:40:34 -0600

    testing_conditional_statements

---
+++ testing_conditional_statements.py
@@ -0,0 +1,17 @@
+x = int(input("What is the first number?"))
+y = int(input("What is the second number?"))
+
+if x > y:
+    print("The first number is greater.")
+if x < y:
+    print("The second number is greater.")
+if x == y:
+    print("The numbers are equal.")
+
+print()
+
+user_animal= input("What is your favorite animal?")
+if user_animal.lower() == "platypus":
+    print("That is my favorite too!")
+else:
+    print("That is not my favorite. Choose a better one!")
