47848c6210657e497e50ce38b58c451b1fcc902f
eliaverett <eliaverett39@gmail.com>
2023-11-21 18:53:35 -0700

    progress on word game

---
+++ word_milestone.py
@@ -0,0 +1,25 @@
+import turtle
+
+print("Welcome to the word guessing game!\n")
+
+def choose_word():
+    secret = 'turtle'
+    return secret
+
+def guess_word(secret):
+    guess_count = 0
+
+    while True:
+
+        user_guess = input("What is your guess?").lower()
+        guess_count += 1
+        if user_guess == secret:
+            print(f"Your guess was correct! You guessed in {guess_count} guesses!")
+            return
+        else:
+            print('Your guess was incorrect. Try Again!')
+        
+
+secret_word = choose_word()
+guess_word(secret_word)
+        
