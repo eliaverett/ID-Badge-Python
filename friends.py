0f7de4cf3e33e0ff35c7f9b7a66c63964c362251
eliaverett <eliaverett39@gmail.com>
2023-11-27 11:48:01 -0700

    updates

---
+++ friends.py
@@ -0,0 +1,16 @@
+friends=[]
+
+name = "none"
+
+while name != "end":
+    name = input("Type the name of a friend: ")
+
+    if name != "end":
+        friends.append(name)
+
+print()
+print("Your friends are: ")
+
+for friend in friends:
+    print(friend)
+
